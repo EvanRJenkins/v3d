136f7e750852646a16127af5cf5e7ceb14a9daab
Evan Jenkins <JenkinsE@jbu.edu>
2025-11-25 17:03:00 -0600

    Add files via upload

---
+++ v3d_functions.h
@@ -0,0 +1,45 @@
+#ifndef V3D_FUNCTIONS_H
+#define V3D_FUNCTIONS_H
+
+#include <math.h>
+
+void v3d_add(v3d_t *v1, v3d_t *v2, v3d_t *vresult)  // Add two vectors
+{
+    vresult->x = v1->x + v2->x;
+    vresult->y = v1->y + v2->y;
+    vresult->z = v1->z + v2->z;
+}
+
+void v3d_sub(v3d_t *v1, v3d_t *v2, v3d_t *vresult)  // Subtract two vectors
+{
+    vresult->x = v1->x - v2->x;
+    vresult->y = v1->y - v2->y;
+    vresult->z = v1->z - v2->z;
+}
+
+double v3d_mag(v3d_t *v1)   // Return magnitude (absolute value) of one vector
+{
+    double j = ((v1->x * v1->x) + (v1->y*v1->y) + ((v1->z*v1->z)));
+    return sqrt(j);
+}
+
+double v3d_dot(v3d_t *v1, v3d_t *v2)    // Return dot product of two vectors
+{
+    return (v1->x * v2->x) + (v1->y * v2->y) + (v1->z * v2->z);
+}
+
+void v3d_cross(v3d_t *v1, v3d_t *v2, v3d_t * vresult)   // Cross product of two vectors
+{                                                       // No return, store at vresult
+    vresult->x = (v1->y * v2->z) - (v1->z * v2->y);
+    vresult->y = (v1->z * v2->x) - (v1->x * v2->z);
+    vresult->z = (v1->x * v2->y) - (v1->y * v2->x);
+}
+
+double v3d_angle(v3d_t *v1, v3d_t *v2)  // Return angle between two vectors (in radians)
+{
+    double numerator = v3d_dot(v1, v2);
+    double denominator = v3d_mag(v1) * v3d_mag(v2);
+    return acos(numerator / denominator);
+}
+
+#endif  // V3D_FUNCTIONS_H
---
+++ v3d_types.h
@@ -0,0 +1,11 @@
+#ifndef V3D_TYPES_H
+#define V3DTYPES_H
+
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {    // 3d vector type
+   double x, y, z;
+} v3d_t;
+
+#endif  // V3D_TYPES_H
